a4029ec30a2fc3463732ca5adc79e8bdf9d6f23e
Lineu Felipe <lineufelipe@gmail.com>
2017-04-17 12:36:49 -0300

    first blood

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+    proto "github.com/golang/protobuf/proto"
+    tvshowpb "github.com/darksasori/tvshow/tvshowpb"
+    "log"
+    "io/ioutil"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+
+func persist(item *tvshowpb.TvShow, collection *mgo.Collection) {
+    query := collection.Find(bson.M{
+        "title": item.GetTitle(),
+        "start": item.GetStart(),
+        "channel.name": item.GetChannel().GetName(),
+    })
+    if count, _ := query.Count(); count > 0 {
+        fmt.Println(item.GetTitle(), " Skiped")
+        return
+    }
+
+    err := collection.Insert(item)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(item.GetTitle(), " Inserted")
+}
+
+func main() {
+    session, err := mgo.Dial("mongodb://tvshow:Shae4bah@ds157320.mlab.com:57320/tvshow")
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+
+    http.HandleFunc("/protobuf", func(w http.ResponseWriter, r *http.Request) {
+        defer r.Body.Close()
+        item := new(tvshowpb.TvShow)
+        data, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+            log.Fatalln("Error", err)
+            return
+        }
+
+        if err := proto.Unmarshal(data, item); err != nil {
+            log.Fatalln("Error", err)
+            return
+        }
+
+        go persist(item, session.DB("tvshow").C("shows"))
+    })
+
+    fmt.Println("Connected to mongodb, wait for connections")
+    http.ListenAndServe(":8000", nil)
+}
