898c8d3e9481cae357d867d276f8f2f55c944359
Lineu Felipe <lineufelipe@gmail.com>
2017-04-18 17:53:31 -0300

    Just a test

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello world")
     })
+    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Only a test")
+    })
 
     fmt.Println("Connected to mongodb, wait for connections")
     http.ListenAndServe(":8080", nil)
