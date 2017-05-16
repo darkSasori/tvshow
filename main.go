f1fefc15cb8f18bee90b1aaff7de033ad34d0582
Lineu Felipe <lineufelipe@gmail.com>
2017-05-15 23:19:11 -0300

    change port to 8080

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func main() {
     http.HandleFunc("/import", ImportHandler)
     http.HandleFunc("/", HelloWorldHandler)
 
-    fmt.Println("Connected to mongodb, wait for connections on :80")
-    http.ListenAndServe(":80", nil)
+    fmt.Println("Connected to mongodb, wait for connections on :8080")
+    http.ListenAndServe(":8080", nil)
 }
